d81d518402cb49f7d9e67879ddee218340290515
NicosNicolaou16 <nicosnicos16@gmail.com>
2020-09-08 16:06:40 +0300

    minor change

--- app/src/main/java/com/nick/sampleroom/database/init_database/BaseDao.kt
+++ app/src/main/java/com/nick/sampleroom/database/init_database/BaseDao.kt
@@ -8,44 +8,5 @@ interface BaseDao<O, L> {
     suspend fun insertOrReplaceObject(data: O)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertOrReplaceNullableObject(data: O?)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertOrReplaceObject(vararg data: O)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrReplaceList(data: L)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAndIgnoreReplaceObject(data: O)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAndIgnoreReplaceNullableObject(data: O?)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAndIgnoreReplaceObject(vararg data: O)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAndIgnoreReplaceList(data: L)
-
-    @Insert
-    suspend fun insertNullableObject(data: O?)
-
-    @Insert
-    suspend fun insertObject(data: O)
-
-    @Insert
-    suspend fun insertObject(vararg data: O)
-
-    @Insert
-    suspend fun insertList(data: L)
-
-    @Update
-    suspend fun updateObject(data: O)
-
-    @Delete
-    suspend fun deleteObject(data: O)
-
-    @Delete
-    suspend fun deleteList(data: L)
 }
