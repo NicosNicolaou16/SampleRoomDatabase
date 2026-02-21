f5bc0a0af79b5accaaf4b437178eacb42acf1bdb
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-21 21:14:09 +0200

    update Kotlin version to 2.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:9.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.6")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
