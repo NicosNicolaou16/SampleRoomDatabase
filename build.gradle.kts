877e2f3bacbba56297b120c10c64288f89bb44a7
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-02-21 21:14:30 +0200

    update KSP version to 2.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
 }
 
 tasks.register("clean", Delete::class) {
