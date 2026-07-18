a1dba2878b3cbee38d09a413150463c9bf45e99a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-19 00:36:48 +0300

    update Core version to 1.19.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ val materialDesignVersion by extra("1.13.0")
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     // Architecture library
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
