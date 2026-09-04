640215425399e50551d6b2ef048bff67e479648a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-09-04 20:52:44 +0300

    update Gradle version to 9.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:9.3.0")
+        classpath("com.android.tools.build:gradle:9.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.8")
         // NOTE: Do not place your application dependencies here; they belong
